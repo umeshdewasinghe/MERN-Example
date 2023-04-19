d5cd3441f090b25e845b2775472651e85d300b0c
umeshdewasinghe <hansithadewasinghe@gmail.com>
2023-04-20 01:45:33 +0530

    db connected

--- MERN_CRUD/server.js
+++ MERN_CRUD/server.js
@@ -1,11 +1,39 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cores = require("cors");
+const dotenv = require("dotenv");
 const app = express();
-
-const port = 8000;
-
+require("dotenv").config();
 
 
-app.listen(port, () => {
-    console.log('server is running ')
+const PORT = process.env.PORT || 8070;
+
+
+app.use(cores());
+app.use(bodyparser.json());
+
+const URL = process.env.DATABASE;
+
+//database connectoin
+mongoose.connect(URL, {
+    useCreateIndex :  true,
+    useNewUrlParser :  true,
+    useUnifiedTopologyL : true,
+    useFindAndModify : false
+ });
+
+const connection = mongoose.connection;
+
+mongoose.connect(URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+//connecting to the port
+app.listen(PORT, () => {
+    console.log('server is running ');
 });
